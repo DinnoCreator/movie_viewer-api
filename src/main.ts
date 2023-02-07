ffad633381c64d36dba5a1a72f2ed114bfc7d2f9
DinnoCreator <97753291+DinnoCreator@users.noreply.github.com>
2023-02-07 10:39:22 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const corsOptions = {optionsSuccessStatus: 200, Credential:true, origin:'http://localhost:3000',};
+  const corsOptions = {optionsSuccessStatus: 200, Credential:true, origin:'https://movie-viewer.onrender.com',};
   app.enableCors(corsOptions);
   await app.listen(3333);
 }
